5f9be23dcc23bb64ea4bfb1a869ccd237345dcf7
Yukih <4rnold.richard@gmail.com>
2025-04-10 09:53:35 +0300

    Enabled raw mode and disabled ECHO

--- WriteMe/WriteMe.c
+++ WriteMe/WriteMe.c
@@ -1,3 +1,22 @@
+#include <termios.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void enableRawMode() {
+	struct termios raw;
+
+	tcgetattr(STDIN_FILENO, &raw); /* Fetch currernt terminal settings */
+
+	raw.c_lflag &= ~(ECHO); /* Turn off ECHO */
+
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw); /* Updates terminal settings */
+}
+
 int main() {
-	return (0);
+	enableRawMode();
+
+	char c;
+	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+	
+	return EXIT_SUCCESS;
 }
